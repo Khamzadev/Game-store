a8dbf747151de0a2bb7fb675ffc52046af58b953
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-10-02 14:22:10 +0600

    add and last commit

---
+++ src/page/Order-page/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import OrderItem from '../../components/Order-Item';
+import { calcTotalPrice } from '../../components/utils';
+
+const OrderPage = () => {
+
+  const items = useSelector((state) => state.cart.itemInCart)
+  console.log(items)
+
+  if(items.length < 0){
+    return <h1>Ваша корзина пуста</h1>
+  }
+
+  return (
+    <div className="order-page">
+      <div className="order-page__left">
+        {items.map(game => <OrderItem game={game}/>)}
+      </div>
+      <div className="order-page__right">
+        <div className="order-page__total-price" style={{fontSize:"30px", fontWeight:"bold"}}>
+         {items.length} товаров на сумму ( {calcTotalPrice(items)}) руб.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderPage;
